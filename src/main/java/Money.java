b3f8f8dc68072f2478b03686c93e0a251d2e1eb6
prachi.more1 <prachi.more@integral.io>
2020-06-04 11:37:53 -0400

    Removed SubClass use in tests by using  Factory methods to create subclass instances from superClass.

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,11 +1,21 @@
 package main.java;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
         return (this.amount == money.amount)
                 && getClass().equals(money.getClass());
     }
+
+    public abstract Money times(int multiplier);
 }
