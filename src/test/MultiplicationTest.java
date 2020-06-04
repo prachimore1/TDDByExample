be61908a00699a102a796b1574c06b21ae3206d0
prachi.more1 <prachi.more@integral.io>
2020-06-04 11:10:04 -0400

    Tested equality by comparing Dollars to Francs. Getting class this way is smelly. We need to fetch currency somehow.

--- src/test/MultiplicationTest.java
+++ src/test/MultiplicationTest.java
@@ -21,11 +21,13 @@ public class MultiplicationTest {
         assertEquals(new Franc(15), five.times(3));
     }
 
+    @SuppressWarnings("SimplifiableJUnitAssertion")
     @Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 }
