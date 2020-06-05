679c77c3a58e21bb5d953a6707a8eb18c2791328
prachi.more1 <prachi.more@integral.io>
2020-06-05 19:54:45 -0400

    Revert "Removed dollar & franc method."

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -9,6 +9,14 @@ public class Money {
         this.currency = currency;
     }
 
+    public static Money dollar(int amount) {
+        return new Dollar(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount, "CHF");
+    }
+
     public boolean equals(Object object) {
         Money money = (Money) object;
         return this.amount == money.amount
