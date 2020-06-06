3e8a6e1de90825152a1e7661ea2a0cda69a175b8
prachi.more1 <prachi.more@integral.io>
2020-06-05 20:03:59 -0400

    Added Addition functionality.

--- src/test/MultiplicationTest.java
+++ src/test/MultiplicationTest.java
@@ -43,4 +43,9 @@ public class MultiplicationTest {
         assertFalse(new Money(10, "USD").equals(new Franc(10, "CHF")));
     }
 
+    @Test
+    public void testSimpleAddition() {
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
+    }
 }
